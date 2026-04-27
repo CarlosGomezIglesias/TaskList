7de53a3124b15a2b3bc2960bf758585bb69f06e1
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-04-27 18:58:57 +0200

    Creado data Task con los valores, id, titulo y hecho

--- app/src/main/java/com/programa1/tasklist/activities/MainActivity.kt
+++ app/src/main/java/com/programa1/tasklist/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.programa1.tasklist
+package com.programa1.tasklist.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.programa1.tasklist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/programa1/tasklist/data/Task.kt
@@ -0,0 +1,8 @@
+package com.programa1.tasklist.data
+
+data class Task (
+    val id: Int,
+    val title: String,
+    val done: Boolean
+){
+}
